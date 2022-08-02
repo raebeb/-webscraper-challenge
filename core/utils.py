3fea9695b6fe5beb0d2f2538bbfbd6e035b37173
Francisca Osores <fosores@linets.cl>
2022-08-01 23:44:26 -0400

    UPDATE get_books method to save books in database

--- core/utils.py
+++ core/utils.py
@@ -1,8 +1,26 @@
 from bs4 import BeautifulSoup
 import requests
 import csv
+import logging
+
+from core.models import Book
+
+def create_soup(url):
+    """
+    Create a BeautifulSoup object from a url
+
+    Args:
+        url (str): url to make request to
+
+    Returns:
+        soup (BeautifulSoup): BeautifulSoup object generated from response
+    """
+    response = requests.get(url)
+    soup = BeautifulSoup(response.text, 'html.parser')
+    return soup
 
 def get_books():
+    
     """
     Get all books from a page
     """
@@ -12,42 +30,33 @@ def get_books():
         print(f'PAGE {page}')
 
         query_parameter = f'catalogue/page-{page}.html'
-        full_url = base_url + query_parameter
-        print(type(full_url))
-        response = requests.get(full_url)
-        soup = BeautifulSoup(response.text, 'html.parser')
+        full_url = f'{base_url}{query_parameter}'
+        soup = create_soup(full_url)
         books = soup.find_all('article')
 
         for book in books:
             print('*** START BOOK ***')
-            #print(f'RESPONSE =====> {book}')
+            new_book = Book()
+
             book_query_parameter = book.find('a')['href']
-            book_title = book.find('h3').text
-            book_cover = book.find(class_='thumbnail')['src']
-            print(f'cover ====> {book_cover}')
-            print(f'Title =====> {book_title}')
-            print(f'url =====> {book_query_parameter}')
-            book_full_url = base_url + 'catalogue/' + book_query_parameter
-            response = requests.get(book_full_url)
-            soup = BeautifulSoup(response.text, 'html.parser')
-            price = soup.find(class_='price_color').text
-            print(f'price ===> {price}')
-            stock = soup.find(class_='instock availability').text
-            print(f'STOCK ==> {stock}')
-            category = soup.find_all('li')[2].text
-            print(f'CATEGORY ===> {category}')
+            book_full_url = f'{base_url}catalogue/{book_query_parameter}'
+            soup = create_soup(book_full_url)
 
-            ### PRODUCT INFO
-
+            new_book.title = soup.find('h1').text
+            new_book.price = (soup.find(class_='price_color').text).replace('Â', '')
+            new_book.stock = (soup.find(class_='instock availability').text).strip()
+            new_book.category = (soup.find_all('li')[2].text).strip()
+            new_book.cover = f"{base_url}{book.find(class_='thumbnail')['src']}"
             table = soup.find_all('tr')
-            upc = table[0].text
-            product_type = table[1].find('td').text
-            price_excl_tax = table[2].find('td').text
-            price_incl_tax = table[3].find('td').text
-            tax = table[4].find('td').text
-            availabililty = table[5].find('td').text
-            number_of_reviews = table[6].find('td').text
+            new_book.upc = (table[0].text).strip()
+            new_book.product_type = table[1].find('td').text
+            new_book.price_excl_tax = (table[2].find('td').text).replace('Â', '')
+            new_book.price_incl_tax = (table[3].find('td').text).replace('Â', '')
+            new_book.tax = (table[4].find('td').text).replace('Â', '')
+            new_book.availability = table[5].find('td').text
+            new_book.number_of_reviews = int(table[6].find('td').text)
+            new_book.save()
 
-            print(f'TABLE ===> {number_of_reviews}')
+            print('*** BOOK SAVED! ***')
 
-            print('*** END BOOK ***')
+
