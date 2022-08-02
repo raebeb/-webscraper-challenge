fa4a0fbae49fdf2ac9fcfc88b5a2100745c22e9c
Francisca Osores <fosores@linets.cl>
2022-08-01 23:43:37 -0400

    ADD book model to admin site

--- core/admin.py
+++ core/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
+from core.models import Book
+
+class BookAdmin(admin.ModelAdmin):
+    pass
+
+admin.site.register(Book, BookAdmin)
 
 # Register your models here.
