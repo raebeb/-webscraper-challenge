679e8067a38401ad646b4671df89fec39cd7ae01
Francisca Osores <fosores@linets.cl>
2022-08-03 00:01:08 -0400

    ADD url to download file

--- core/urls.py
+++ core/urls.py
@@ -2,6 +2,9 @@ from django.urls import path
 
 from . import views
 
+app_name = 'core'
+
 urlpatterns = [
     path('', views.index, name='index'),
+    path('getfile/', views.create_csv, name='getfile')
 ]
