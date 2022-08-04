58cf8e5d6ae305623aa0e71267fd578117b82e6e
Francisca Osores <fosores@linets.cl>
2022-08-04 11:55:36 -0400

    UPDATE static settings

--- webscrapper_challenge/settings.py
+++ webscrapper_challenge/settings.py
@@ -125,9 +125,9 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, 'core/static'),
+   os.path.join(BASE_DIR, 'core/static/')
 ]
-STATIC_ROOT = os.path.join(BASE_DIR, 'static_root')
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
